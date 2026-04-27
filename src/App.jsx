55ba53dee8b4061d15a30b796edddd039db2dc90
Jordan A. Cabandon <cabandonjordan@gmail.com>
2026-04-27 21:52:59 +0800

    feat: implement OS-themed tech modules with 3D hover links

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import SystemInit from './components/SystemInit';
 import TechStack from './components/TechStack';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-      <SystemInit />
-      <TechStack />
+    <div className="main-viewport">
+      {/* Background Layers */}
+      <div className="css-grid-bg"></div>
+      <div className="css-vignette"></div>
+      
+      {/* Portfolio Content */}
+      <div className="content-layer">
+        <Navbar />
+        <SystemInit />
+        <TechStack />
+      </div>
     </div>
   );
 }
