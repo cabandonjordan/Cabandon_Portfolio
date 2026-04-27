d197980a86fd41a99b9b5703043e747d3273ae33
Jordan A. Cabandon <cabandonjordan@gmail.com>
2026-04-27 20:50:26 +0800

    feat: Added a navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-brand">
+        <span className="brand-icon">⚡</span>
+        <span className="brand-name">CABANDON_OS</span>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#about">[ INIT ]</a></li>
+        <li><a href="#stack">[ STACK ]</a></li>
+        <li><a href="#projects">[ ARCHIVE ]</a></li>
+        <li><a href="#contact">[ COMMS ]</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
